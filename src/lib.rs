85cb7fd643903d04ccd17d39a8dd051500d2311c
kinire98 <iknitgarri@outlook.com>
2023-12-20 14:27:22 +0100

    Added getters and setters

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,60 @@
-use std::fs::File;
+use std::{fs::File, pin};
 /*
 https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants
 */
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 pub struct Props {
-    read_only: bool, // 1 -> bit 1
-    hidden: bool, // 2 -> bit 2
-    system: bool, // 4 -> bit 3
-    directory: bool, // 16 -> bit 5
-    archive: bool, // 32 -> bit 6
-    device: bool, // 64 -> bit 7 
-    normal: bool, // 128 -> bit 8
-    temporary: bool, // 256 -> bit 9
-    sparse: bool, // 512 -> bit 10
-    reparse: bool, // 1024 -> bit 11
-    compressed: bool, // 2048 -> bit 12
-    offline: bool, // 4096 -> bit 13
-    not_content_indexed: bool, // 8192 -> bit 14
-    encrypted: bool, // 16384 -> bit 15
-    integrity_stream: bool, // 32768 -> bit 16
-    virtual_file: bool, //For system reserved use 65536 -> bit 17
-    no_scrub_data: bool, // 131072 -> bit 18
-    extended_attributes: bool, // 262144 -> bit 19
-    pinned: bool, // 524288 -> bit 20
-    unpinned: bool, // 1048576 -> bit 21
-    recall_on_open: bool, // 262144 -> bit 22
+    read_only: bool,             // 1 -> bit 1
+    hidden: bool,                // 2 -> bit 2
+    system: bool,                // 4 -> bit 3
+    directory: bool,             // 16 -> bit 5
+    archive: bool,               // 32 -> bit 6
+    device: bool,                // 64 -> bit 7
+    normal: bool,                // 128 -> bit 8
+    temporary: bool,             // 256 -> bit 9
+    sparse: bool,                // 512 -> bit 10
+    reparse: bool,               // 1024 -> bit 11
+    compressed: bool,            // 2048 -> bit 12
+    offline: bool,               // 4096 -> bit 13
+    not_content_indexed: bool,   // 8192 -> bit 14
+    encrypted: bool,             // 16384 -> bit 15
+    integrity_stream: bool,      // 32768 -> bit 16
+    virtual_file: bool,          //For system reserved use 65536 -> bit 17
+    no_scrub_data: bool,         // 131072 -> bit 18
+    extended_attributes: bool,   // 262144 -> bit 19
+    pinned: bool,                // 524288 -> bit 20
+    unpinned: bool,              // 1048576 -> bit 21
+    recall_on_open: bool,        // 262144 -> bit 22
     recall_on_data_access: bool, // 4194304 -> bit 23
-    summarized: bool
+    summarized: bool,
 }
 impl Props {
     pub fn new() -> Self {
-        Props { read_only: false, hidden: false, system: false, directory: false, archive: false, device: false, normal: false, temporary: false, sparse: false, reparse: false, compressed: false, offline: false, not_content_indexed: false, encrypted: false, integrity_stream: false, virtual_file: false, no_scrub_data: false, extended_attributes: false, pinned: false, unpinned: false, recall_on_open: false, recall_on_data_access: false, summarized: false }
+        Props {
+            read_only: false,
+            hidden: false,
+            system: false,
+            directory: false,
+            archive: false,
+            device: false,
+            normal: false,
+            temporary: false,
+            sparse: false,
+            reparse: false,
+            compressed: false,
+            offline: false,
+            not_content_indexed: false,
+            encrypted: false,
+            integrity_stream: false,
+            virtual_file: false,
+            no_scrub_data: false,
+            extended_attributes: false,
+            pinned: false,
+            unpinned: false,
+            recall_on_open: false,
+            recall_on_data_access: false,
+            summarized: false,
+        }
     }
     pub fn from_number(props: u32) -> Self {
         let mut props = props;
@@ -207,8 +232,9 @@ impl Props {
         props <<= 1;
         if clone != props {
             recall_on_data_access = true;
-        } 
-        Props { read_only,
+        }
+        Props {
+            read_only,
             hidden,
             system,
             directory,
@@ -217,30 +243,175 @@ impl Props {
             normal,
             temporary,
             sparse,
-            reparse, 
-            compressed, 
-            offline, 
-            not_content_indexed, 
+            reparse,
+            compressed,
+            offline,
+            not_content_indexed,
             encrypted,
             integrity_stream,
             virtual_file,
-            no_scrub_data, 
+            no_scrub_data,
             extended_attributes,
             pinned,
             unpinned,
             recall_on_open,
             recall_on_data_access,
-            summarized: false }
+            summarized: false,
+        }
     }
-    pub fn from_file(file: &File) -> Self {
-        Self::from_number(1)
+    pub fn from_file(_file: &File) -> Self {
+        todo!()
+    }
+    pub fn is_read_only(&self) -> bool {
+        self.read_only
+    }
+    pub fn read_only(&mut self, read_only: bool) {
+        self.read_only = read_only;
+    }
+    pub fn is_hidden(&self) -> bool {
+        self.hidden
+    }
+    pub fn hidden(&mut self, hidden: bool) {
+        self.hidden = hidden;
+    }
+    pub fn is_system(&self) -> bool {
+        self.system
+    }
+    pub fn system(&mut self, system: bool) {
+        self.system = system;
+    }
+    pub fn is_archive(&self) -> bool {
+        self.archive
+    }
+    pub fn archive(&mut self, _archive: bool) -> Result<(), ()>{
+        todo!()
+    }
+    pub fn is_directory(&self) -> bool {
+        self.read_only
+    }
+    pub fn directory(&mut self, _directory: bool) -> Result<(), ()>{
+        todo!()
+    }
+    pub fn is_device(&self) -> bool {
+        self.device
+    }
+    pub fn device(&mut self, device: bool) {
+        self.device = device;
+    }
+    pub fn is_normal(&self) -> bool {
+        self.normal
+    }
+    pub fn normal(&mut self, normal: bool) {
+        self.normal = normal;
+    }
+    pub fn is_temporary(&self) -> bool {
+        self.temporary
+    }
+    pub fn temporary(&mut self, temporary: bool) {
+        self.temporary = temporary;
+    }
+    pub fn is_sparse(&self) -> bool {
+        self.sparse
+    }
+    pub fn sparse(&mut self, sparse: bool) {
+        self.sparse = sparse;
+    }
+    pub fn is_reparse(&self) -> bool {
+        self.reparse
+    }
+    pub fn reparse(&mut self, reparse: bool) {
+        self.reparse = reparse;
+    }
+    pub fn is_compressed(&self) -> bool {
+        self.compressed
+    }
+    pub fn compressed(&mut self, compressed: bool) {
+        self.compressed = compressed;
+    }
+    pub fn is_offline(&self) -> bool {
+        self.offline
+    }
+    pub fn offline(&mut self, offline: bool) {
+        self.offline = offline;
+    }
+    pub fn is_not_content_indexed(&self) -> bool {
+        self.not_content_indexed
+    }
+    pub fn not_content_indexed(&mut self, not_content_indexed: bool) {
+        self.not_content_indexed = not_content_indexed;
+    }
+    pub fn is_encrypted(&self) -> bool {
+        self.encrypted
+    }
+    pub fn encrypted(&mut self, encrypted: bool) {
+        self.encrypted = encrypted;
+    }
+    pub fn is_integrity_stream(&self) -> bool {
+        self.integrity_stream
+    }
+    pub fn integrity_stream(&mut self, integrity_stream: bool) {
+        self.integrity_stream = integrity_stream;
+    }
+    pub fn is_virtual_file(&self) -> bool {
+        self.virtual_file
+    }
+    pub fn virtual_file(&mut self, virtual_file: bool) {
+        self.virtual_file = virtual_file;
+    }
+    pub fn is_no_scrub_data(&self) -> bool {
+        self.no_scrub_data
+    }
+    pub fn no_scrub_data(&mut self, no_scrub_data: bool) {
+        self.no_scrub_data = no_scrub_data;
+    }
+    pub fn is_extended_attributes(&self) -> bool {
+        self.extended_attributes
+    }
+    pub fn extended_attributes(&mut self, extended_attributes: bool) {
+        self.extended_attributes = extended_attributes;
+    }
+    pub fn is_pinned(&self) -> bool {
+        self.pinned
+    }
+    pub fn pinned(&mut self, pinned: bool) {
+        self.pinned = pinned;
+    }
+    pub fn is_unpinned(&self) -> bool {
+        self.unpinned
+    }
+    pub fn unpinned(&mut self, unpinned: bool) {
+        self.unpinned = unpinned;
+    }
+    pub fn is_recall_on_open(&self) -> bool {
+        self.recall_on_open
+    }
+    pub fn recall_on_open(&mut self, recall_on_open: bool) {
+        self.recall_on_open = recall_on_open;
+    }
+    pub fn is_recall_on_data_access(&self) -> bool {
+        self.recall_on_data_access
+    }
+    pub fn recall_on_data_access(&mut self, recall_on_data_access: bool) {
+        self.recall_on_data_access = recall_on_data_access;
+    }
+    pub fn is_summarized(&self) -> bool {
+        self.summarized
+    }
+    pub fn summarized(&mut self, summarized: bool) {
+        self.summarized = summarized;
     }
 }
-   
-   
-   
-   
-   
+impl Default for Props {
+fn default() -> Self {
+        Self::new()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::Props;
 
-
-
+    #[test]
+    fn test_basic() {
+        assert_eq!(Props::from_number(0), Props::default());
+    }
+}
