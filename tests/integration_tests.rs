f8689b71135e5e837a79a1fcc7ce8de46d4f1f2d
kinire98 <iknitgarri@outlook.com>
2023-12-28 13:10:41 +0100

    Some integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,1 +1,17 @@
 use keypropdecode::Props;
+#[test]
+fn read_only_and_hidden_archive_from_number() {
+    let mut props = Props::default();
+    props.archive(true).unwrap();
+    props.read_only(true);
+    props.hidden(true);
+    assert_eq!(props, Props::from_number(0b100011))
+}
+#[test]
+fn hidden_system_directory_string() {
+    let mut props = Props::default();
+    props.directory(true).unwrap();
+    props.hidden(true);
+    props.system(true);
+    assert_eq!(props.to_string(), "d--hs-".to_string())
+}
